8d1036e0a14f53b367be5c206eb1929976bbb08a
Saurabh2993 <saurabhghadi@gmail.com>
2017-05-30 20:22:14 -0700

    Move zeroes - Leetcode easy

---
+++ Leetcode/leetcode_easy/Move_Zeroes.java
@@ -0,0 +1,30 @@
+package leetcode_easy;
+
+public class Move_Zeroes {
+	
+	static int nums[] = {0, 0, 1};
+	
+	public static void main(String args[]) {
+		
+		moveZeroes(nums);
+		
+	}
+	
+	public static void moveZeroes(int[] nums) {
+		
+		int pos = 0;
+		
+		for(int i : nums) {
+			if(i != 0) {
+				nums[pos++] = i;
+			}
+		}
+		while(pos < nums.length)
+			nums[pos++] = 0;
+		
+		for (int i = 0; i < nums.length; i++) {
+			System.out.print(nums[i] + " ");
+		}
+			
+	}
+}
