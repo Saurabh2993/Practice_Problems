41a4dcbbd71db576c9573c1dc7735a7c6bf93e4c
Saurabh2993 <saurabhghadi@gmail.com>
2017-06-06 13:37:44 -0700

    Search String - Leetcode easy

---
+++ Leetcode/leetcode_easy/Search_String.java
@@ -0,0 +1,37 @@
+package leetcode_easy;
+
+public class Search_String {
+	
+	public static void main(String args[]) {
+		
+		String x1 = "Hi all";
+		String x2 = "all";
+		
+		System.out.println(strStr(x1, x2));
+		
+		
+	}
+	
+	public static int strStr(String haystack, String needle) {
+		
+		int l1 = haystack.length();
+		int l2 = needle.length();
+		
+//		System.out.println(l1 + " " + l2);
+		
+		if(l2 == 0)
+			return 0;
+		if(l1 < l2)
+			return -1;
+		
+		for (int i = 0; i <= l1 - l2; i++) {
+//			System.out.println(haystack.substring(i, i + l2));
+			if(haystack.substring(i, i + l2).equals(needle))
+				return i;
+		}
+		
+		return -1;
+		
+	}
+
+}
