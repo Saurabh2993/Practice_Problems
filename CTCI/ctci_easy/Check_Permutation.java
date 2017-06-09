6886ba1bd6d0a5b652033e8af9457d659d2050e1
Saurabh2993 <saurabhghadi@gmail.com>
2017-06-09 16:11:56 -0700

    CTCI Easy - Check Permutations

---
+++ CTCI/ctci_easy/Check_Permutation.java
@@ -0,0 +1,51 @@
+package ctci_easy;
+
+import java.util.HashMap;
+
+public class Check_Permutation {
+	
+	public static void main(String args[]) {
+		
+		String s = "aaabacc";
+		String t = "bcabcaa";
+		
+		System.out.println(checkPermutation(s, t));
+		
+	}
+	
+	public static boolean checkPermutation(String s, String t) {
+		
+		if(s.length() != t.length())
+			return false;
+		
+		HashMap<Character, Integer> char_map = new HashMap<>();
+		
+		for (int i = 0; i < s.length(); i++) {
+			char curr = s.charAt(i);
+			if(char_map.containsKey(curr)) {
+				char_map.put(curr, char_map.get(curr) + 1);
+			} else {
+				char_map.put(curr, 1);
+			}
+		}
+		
+		for (int i = 0; i < t.length(); i++) {
+			char curr = t.charAt(i);
+			
+			if(char_map.containsKey(curr)) {
+				System.out.println(curr + " " + char_map.get(curr));
+				if(char_map.get(curr) > 0) {
+					char_map.put(curr, char_map.get(curr) - 1);
+				} else {
+					return false;
+				}
+			} else {
+				return false;
+			}
+		}
+		
+		return true;
+		
+	}
+
+}
