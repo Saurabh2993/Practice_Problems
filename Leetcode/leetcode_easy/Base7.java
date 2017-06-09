d793f14e0a0121294082587581b7bfd592c5d849
Saurabh2993 <saurabhghadi@gmail.com>
2017-06-08 17:37:26 -0700

    Base7 - Leetcode easy

---
+++ Leetcode/leetcode_easy/Base7.java
@@ -0,0 +1,36 @@
+package leetcode_easy;
+
+public class Base7 {
+	
+	public static void main(String args[]) {
+		
+		int x = 0;
+		
+		convertToBase7(x);
+		
+	}
+	
+	public static String convertToBase7(int num) {
+		
+		String ans = "";
+		boolean negative = false;
+		
+		if(num < 0) {
+			negative = true;
+			num = -num;
+		}
+		
+		do {
+			ans = (num % 7) + ans;
+			num = num / 7;
+		} while (num != 0);
+		
+		if(negative)
+			ans = "-" + ans;
+		
+		System.out.println(ans);
+			return ans;
+		
+	}
+
+}
