787cd8c5fac210af909de8cb37549555c6c58bec
Saurabh2993 <saurabhghadi@gmail.com>
2017-06-11 15:17:01 -0700

    COmpress Strings - CTCI

---
+++ CTCI/ctci_easy/Compress_Strings.java
@@ -0,0 +1,37 @@
+package ctci_easy;
+
+public class Compress_Strings {
+	
+	public static void main(String args[]) {
+		
+		System.out.println(compress("aaabbbbbb"));
+		
+	}
+	
+	public static String compress(String s) {
+		
+		if(s.length() == 1)
+			return s;
+		
+		StringBuilder sb = new StringBuilder();
+		int count = 1;
+		sb.append(s.charAt(0));
+		
+		for (int i = 1; i < s.length(); i++) {
+			if(sb.length() >= s.length())
+				return s;
+			
+			if(s.charAt(i) == s.charAt(i - 1)) {
+				count ++;
+			} else {
+				sb.append(count);
+				count = 1;
+				sb.append(s.charAt(i));
+			}
+		}
+		sb.append(count);
+		
+//		System.out.println("--------------------");
+		return sb.length() < s.length() ? sb.toString() : s;
+	}
+}
