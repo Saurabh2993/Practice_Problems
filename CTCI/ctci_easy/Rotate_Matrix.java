49501248a76e411e7cffaaae93fd6e2c3e35f045
Saurabh2993 <saurabhghadi@gmail.com>
2017-06-30 09:09:19 -0700

    Rotate Matrix - minor change

--- CTCI/ctci_easy/Rotate_Matrix.java
+++ CTCI/ctci_easy/Rotate_Matrix.java
@@ -6,43 +6,56 @@ public class Rotate_Matrix {
 		
 		int[][] matrix = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
 		
-		rotateMatrix(matrix);
-	}
-	
-	public static void rotateMatrix(int[][] matrix) {
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[0].length; j++) {
+				System.out.print(matrix[i][j] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println("------------------------------------------");
 		
-		if(!(matrix.length == 0 || matrix.length !=  matrix[0].length)) {
-			
-			int n = matrix.length;
-			
-			for (int layer = 0; layer < n / 2; layer++) {
-				int first = layer;
-				int last = n - layer - 1;
-				for (int i = first; i < last; i++) {
-					
-					int offset = i - first;
-					int top = matrix[first][i];
-					
-					matrix[first][i] = matrix[last - offset][first];
-					
-					matrix[last - offset][first] = matrix[last][last - offset];
-					
-					matrix[last][last - offset] = matrix[i][last];
-					
-					matrix[i][last] = top;
-					
-				}
+		rotateMatrix(matrix);
+		
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[0].length; j++) {
+				System.out.print(matrix[i][j] + " ");
 			}
-			
-			for (int i = 0; i < matrix.length; i++) {
-				for (int j = 0; j < matrix[0].length; j++) {
-					System.out.print(matrix[i][j] + " ");
-				}
-				System.out.println();
-			}
-			
-			
+			System.out.println();
 		}
 	}
-
+	
+	public static int[][] rotateMatrix(int[][] matrix) {
+		
+		if(matrix.length == 0 || matrix.length == 1 || matrix.length != matrix[0].length)
+			return matrix;
+			
+		int n = matrix.length;
+		for (int layer = 0; layer < n / 2; layer++) {
+			
+			int first = layer;
+			int last = n - layer - 1;
+			
+			for (int i = first; i < last; i++) {
+				
+				int offset = i - first;			//important
+				int temp = matrix[first][i];
+				
+				//left -> top
+				matrix[first][i] = matrix[last - offset][first];
+				
+				//bottom -> left
+				matrix[last - offset][first] = matrix[last][last - offset];
+				
+				//right -> bottom
+				matrix[last][last - offset] = matrix[i][last];
+				
+				//top -> right
+				matrix[i][last] = temp;
+				
+			}
+		}
+		
+		return matrix;
+	}
+	
 }
