188353a8f1fc837d6bbf0cce6b853a1ccb9040c0
Saurabh2993 <saurabhghadi@gmail.com>
2017-07-01 18:46:30 -0700

    Sort Stack - Elements in the stack are sorted

---
+++ CTCI/stacksAndQueues/SortStack.java
@@ -0,0 +1,52 @@
+package stacksAndQueues;
+
+public class SortStack extends Stack_LL<Integer>{
+	
+	Stack_LL<Integer> stack1;
+	Stack_LL<Integer> stack2;
+	
+	public SortStack() {
+		stack1 = new Stack_LL<>();
+		stack2 = new Stack_LL<>();
+	}
+	
+	public void push(int x) {
+		if(stack1.isEmpty()) {
+			stack1.push(x);
+			return;
+		}
+		if(x <= stack1.peek()) {
+			stack1.push(x);
+		} else {
+			pushToPosition(x);
+		}
+	}
+	
+	public Integer pop() {
+		return stack1.pop();
+	}
+	
+	private void pushToPosition(int x) {
+		
+		while(!stack1.isEmpty()) {
+			if(x > stack1.peek()) {
+				stack2.push(stack1.pop());
+			} else {
+				break;
+			}
+		}
+		stack1.push(x);
+		while(!stack2.isEmpty()) {
+			stack1.push(stack2.pop());
+		}
+	}
+	
+	public Integer peek() {
+		return stack1.peek();
+	}
+	
+	public int getSize() {
+		return stack1.getSize();
+	}
+
+}
---
+++ CTCI/stacksAndQueues/SortStack_Test.java
@@ -0,0 +1,26 @@
+package stacksAndQueues;
+
+public class SortStack_Test {
+	
+	public static void main(String args[]) {
+		
+		SortStack ss = new SortStack();
+		
+		ss.push(5);
+		ss.push(2);
+		System.out.println(ss.peek());
+		ss.push(6);
+		System.out.println(ss.peek());
+		ss.push(14);
+		System.out.println(ss.peek());
+		ss.push(1);
+		System.out.println(ss.peek());
+		
+		System.out.println(ss.pop());
+		System.out.println(ss.pop());
+		System.out.println(ss.pop());
+		System.out.println(ss.pop());
+		System.out.println(ss.pop());
+	}
+
+}
