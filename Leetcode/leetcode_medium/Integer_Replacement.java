9f18800c8d29aff3fb937fbbd3b3c03adb4e5712
Saurabh2993 <saurabhghadi@gmail.com>
2017-07-07 15:46:26 -0700

    Minor change

--- Leetcode/leetcode_medium/Integer_Replacement.java
+++ Leetcode/leetcode_medium/Integer_Replacement.java
@@ -10,6 +10,7 @@ public class Integer_Replacement {
 		
 	}
 	
+//	Logic here is if n is an odd number, (n-1) and (n+1) will be even and one of them will be a multiple of 4. If n is a multiple of 4 we can take at least two steps towards 1 before running into another odd number where as if n is a multiple of 2 we can take only one step and then we would run into another odd number. The only edge case here is for number 3 where it is better if we choose 2 instead of 4.  
 	public int integerReplacement(int n) {
         
         if(n == Integer.MAX_VALUE) {
