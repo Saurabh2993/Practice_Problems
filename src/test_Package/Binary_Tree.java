1a3b4213da57b657e01fe84ef23865e3b599f91d
Saurabh2993 <saurabhghadi@gmail.com>
2017-08-10 21:31:17 -0700

    Flatten Binary Tree to Linked List - Leetcode Medium
    Given a binary tree, flatten it to a linked list in-place.
    
    For example,
    Given
    
             1
            / \
           2   5
          / \   \
         3   4   6
    
    The flattened tree should look like:
       1
        \
         2
          \
           3
            \
             4
              \
               5
                \
                 6

--- src/test_Package/Binary_Tree.java
+++ src/test_Package/Binary_Tree.java
@@ -540,7 +540,26 @@ public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         } else {
             return Math.max(left_height, right_height) + 1;
         }
+    }
+    
+    public void flatten(TreeNode root) {
+        if(root == null) {
+            return;
+        }
         
+        TreeNode left = root.left;
+        TreeNode right = root.right;
         
+        flatten(left);
+        flatten(right);
+        
+        root.left = null;
+        root.right = left;
+        TreeNode curr = root;
+        
+        while(curr.right != null) {
+            curr = curr.right;
+        }
+        curr.right = right;
     }
 }
